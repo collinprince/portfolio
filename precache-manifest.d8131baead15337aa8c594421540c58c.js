8afe0c9db24d825aeea9a90f64616d4974deca98
Collin Prince <collin@Collins-MacBook-Pro-2.local>
2020-04-14 01:35:38 -0500

    Updates

---
+++ precache-manifest.d8131baead15337aa8c594421540c58c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee61baadf19484fd9bc57437feb1bb7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae08a9c293b27f588cc1",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c31f142e634812c1e2b4",
+    "url": "/portfolio/static/js/2.aeeb6567.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.aeeb6567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae08a9c293b27f588cc1",
+    "url": "/portfolio/static/js/main.f54929f6.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "41c062107c2a75d5ac7fcaf22e532f75",
+    "url": "/portfolio/static/media/CollinPrince_Resume.41c06210.pdf"
+  },
+  {
+    "revision": "a81e282caf65b76dce8527fc1d8ec9e9",
+    "url": "/portfolio/static/media/campus.a81e282c.jpg"
+  },
+  {
+    "revision": "56a13d550f75e864ec7cacb414eb9743",
+    "url": "/portfolio/static/media/getty.56a13d55.jpg"
+  },
+  {
+    "revision": "ec68112f66cd5d2b7c281903dc4f8739",
+    "url": "/portfolio/static/media/linkedin.ec68112f.png"
+  },
+  {
+    "revision": "fcb02df7e707068dda88ca1abdd562b5",
+    "url": "/portfolio/static/media/prof.fcb02df7.png"
+  },
+  {
+    "revision": "58b50ddfe64cfd5f6c41df0d07bd437c",
+    "url": "/portfolio/static/media/sit.58b50ddf.jpg"
+  },
+  {
+    "revision": "64f26e0daec066cfa1a68b6355dc49b2",
+    "url": "/portfolio/static/media/sm.64f26e0d.jpg"
+  },
+  {
+    "revision": "458f463c51225bb1afdd1346ac95d25c",
+    "url": "/portfolio/static/media/steps.458f463c.jpg"
+  }
+]);
